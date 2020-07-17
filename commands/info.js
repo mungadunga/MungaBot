0205f11db6a1dcef526878253d00a12d8b424f51
mungadunga <67319507+mungadunga@users.noreply.github.com>
2020-07-17 20:58:43 +0300

    bot(v2.2.1)(updated)

--- commands/info.js
+++ commands/info.js
@@ -9,29 +9,29 @@ module.exports = {
                 .setColor(0x7A7B80)
                 .setTitle("Welcome to MungaBot!")
                 .setDescription(
-                        `
+`
                    
-                         __info commands:__
-                         /info : u just used this command no need to explain lmao
-                         /version : displays current version of the bot
-                         /creator : hmm who created almighty munga bot? 
-                         /date : displays release date of munga bot
-                         /server : sends the link to some servers ( including the bot's official one )
+__info commands:__
+/info : u just used this command no need to explain lmao
+/version : displays current version of the bot
+/creator : hmm who created almighty munga bot? 
+/date : displays release date of munga bot
+/server : sends the link to some servers ( including the bot's official one )
                              
-                         __regular commands:__
-                         /sup : say hello to munga bot! ( mostly used to test if the bot is online lol )
-                         /userinfo : info about user
-                         /howgay : why are you gae?
-                         /iq : how high is your iq?
-                         /poll : create a simple yes or no poll!
-                         /meme : sends a meme from r/meme
-                         /snipe : try this command on someone! And if ur lucky, u may get a crit hit!
+__regular commands:__
+/sup : say hello to munga bot! ( mostly used to test if the bot is online lol )
+/userinfo : info about user
+/howgay : why are you gae?
+/iq : how high is your iq?
+/poll : create a simple yes or no poll!
+/meme : sends a meme from r/meme
+/snipe : try this command on someone! And if ur lucky, u may get a crit hit!
                              
-                         __admin commands:__
-                         /kick : kicks member
-                         /ban : bans member
-                         (do not use these commands on yourself it actually works lol)
-                         `
+__admin commands:__
+/kick : kicks member
+/ban : bans member
+(do not use these commands on yourself it actually works lol)
+`
                         
                 )
                         
