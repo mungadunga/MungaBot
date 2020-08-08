63661c9139edbd0406a8864135678b3cc4c461f4
mungadunga <67319507+mungadunga@users.noreply.github.com>
2020-08-08 15:33:23 +0300

    MungaBot(v4.1.1)
    bot doesnt want to go online

--- commands/update.js
+++ commands/update.js
@@ -5,7 +5,7 @@ module.exports = {
 
         const Discord = require('discord.js');
         
-        var version = '4.1.0'; 
+        var version = '4.1.1'; 
 
         //embed
 
--- commands/version.js
+++ commands/version.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "displays current version of the bot",
     execute(message, args){
 
-        var version = '4.1.0'; 
+        var version = '4.1.1'; 
         const Discord = require('discord.js');
 
         const embed = new Discord.MessageEmbed()
