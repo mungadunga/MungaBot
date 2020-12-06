51e96767f89b024a5bec5191f2aa9b54f4aa68a8
mungadunga <67319507+mungadunga@users.noreply.github.com>
2020-12-06 15:28:41 +0100

    MungaBot v6.4.0

--- commands/clap.js
+++ commands/clap.js
@@ -9,13 +9,10 @@ module.exports = {
 
             const Discord = require('discord.js');
             const embed = new Discord.MessageEmbed()
-    
             .setColor(0xFF0000)
             .addField("Correct usage:", "`>clap` <...>")
             .setFooter("For more info use `>info-clap`");
-            
             message.channel.send(embed);
-
         } else {
             message.delete();
             message.channel.send(' :clap: ' + args.join(' :clap: '));
