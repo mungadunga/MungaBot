0a69dadf904ad4ff6887342590efd9971f3875f6
mungadunga <67319507+mungadunga@users.noreply.github.com>
2020-12-06 15:10:46 +0100

    MungaBot v6.4.0

--- commands/alia.js
+++ commands/alia.js
@@ -2,8 +2,6 @@ module.exports = {
     name: 'alia',
     description: "alia command",
     execute(message, args){
-
         message.channel.send({files: ["./media/alia.mp4"]});
-        
     }
 }
